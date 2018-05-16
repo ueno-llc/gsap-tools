a75a006978979bfdb06d321edb4120d97e6e17ad
Finnur Sigurðsson <finnursigu@gmail.com>
2018-05-16 04:42:38 +0000

    fixed gsap conflict

---
+++ src/app/utils/gsap.js
@@ -0,0 +1,13 @@
+if (typeof window === 'undefined') {
+  module.exports = {};
+} else if (typeof window.TweenLite === 'undefined') {
+  // possibly import gsap here ?
+  console.error('Missing TweenLite');
+} else {
+  module.exports = {
+    TweenLite: window.TweenLite,
+    TweenMax: window.TweenMax,
+    TimelineLite: window.TimelineLite,
+    TimelineMax: window.TimelineMax,
+  };
+}
