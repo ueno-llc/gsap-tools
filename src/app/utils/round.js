694cd605c079feaccf2ae1b687199dbd63497c65
Jérémy Barbet <jeremgraph@gmail.com>
2018-05-25 16:00:38 +0000

    Just toFixed on number

--- src/app/utils/round.js
+++ src/app/utils/round.js
@@ -1,22 +1,5 @@
 function round(number) {
-  if (!number) {
-    return '0.00';
-  }
-
-  const nbr = Math.round(`${number}e2`) + 'e-2'; // eslint-disable-line
-  const { length } = nbr.toString();
-
-  let val;
-
-  if (length === 3) {
-    val = `${nbr}0`;
-  } else if (length === 1) {
-    val = `${nbr}.00`;
-  } else {
-    val = nbr;
-  }
-
-  return Number(val);
+  return number.toFixed(2);
 }
 
 export default round;
