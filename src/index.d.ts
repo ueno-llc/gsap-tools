8949663838a4f860686b260232382b7e6f9ca603
Jérémy Barbet <jeremgraph@gmail.com>
2019-02-11 13:26:27 +0000

    Add definitions

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { TweenLite, TimelineLite } from 'gsap';
+
+interface IGsapTools {
+  onClick?: () => void;
+  isVisible?: boolean;
+  isFixed?: boolean;
+}
+
+export function add(animation: TweenLite | TimelineLite, animationId?: string): GsapTools;
+export default class GsapTools extends React.PureComponent<IGsapTools> {}
